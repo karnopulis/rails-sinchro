dd014abe9462e61d6d5798bdea0641a0edcf048c
karnopulis <doctorsed@list.ru>
2017-01-24 13:27:23 +0000

    sdasdadaD

--- db/migrate/20161216121633_add_result_ref_to_new_pictures.rb
+++ db/migrate/20161216121633_add_result_ref_to_new_pictures.rb
@@ -1,5 +1,7 @@
 class AddResultRefToNewPictures < ActiveRecord::Migration
   def change
     add_reference :new_pictures, :result, index: true, foreign_key: true
+    add_reference :new_pictures, :new_offer, index: true, foreign_key: true
+
   end
 end
