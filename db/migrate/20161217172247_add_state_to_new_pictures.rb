25f9d48a0af7f7356fd8be089cc41ddfb7f6001d
karnopulis <doctorsed@list.ru>
2017-01-24 13:34:23 +0000

    vxcvx

--- db/migrate/20161217172247_add_state_to_new_pictures.rb
+++ db/migrate/20161217172247_add_state_to_new_pictures.rb
@@ -2,7 +2,7 @@ class AddStateToNewPictures < ActiveRecord::Migration
   def change
     add_column :new_pictures, :state, :string
     add_column :new_pictures, :error, :string
-    add_reference :new_pictures, :new_offer, index: true, foreign_key: true
+    add_reference :new_offer, :new_pictures, index: true, foreign_key: true
 
   end
 end
