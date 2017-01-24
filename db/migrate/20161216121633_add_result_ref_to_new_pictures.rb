253b590b3ab9d948f26785aa48be57d3e9f63928
karnopulis <doctorsed@list.ru>
2017-01-24 13:33:01 +0000

    FGDF

--- db/migrate/20161216121633_add_result_ref_to_new_pictures.rb
+++ db/migrate/20161216121633_add_result_ref_to_new_pictures.rb
@@ -1,7 +1,6 @@
 class AddResultRefToNewPictures < ActiveRecord::Migration
   def change
     add_reference :new_pictures, :result, index: true, foreign_key: true
-    add_reference :new_pictures, :new_offer, index: true, foreign_key: true
 
   end
 end
--- db/migrate/20161217172247_add_state_to_new_pictures.rb
+++ db/migrate/20161217172247_add_state_to_new_pictures.rb
@@ -2,5 +2,7 @@ class AddStateToNewPictures < ActiveRecord::Migration
   def change
     add_column :new_pictures, :state, :string
     add_column :new_pictures, :error, :string
+    add_reference :new_pictures, :new_offer, index: true, foreign_key: true
+
   end
 end
